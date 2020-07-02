e2ea0e8321ec41847ec1bea5cb84a49285107f79
technosoul <rockdrigo.mtz@gmail.com>
2020-07-01 22:55:19 -0500

    string concatenation n_n

--- name.py
+++ name.py
@@ -2,3 +2,8 @@ name = "ada lovelace"
 print(name.title())
 print(name.upper())
 print(name.lower())
+
+firts_name = "ada"
+last_name = "lovelace"
+full_name = firts_name + " " + last_name
+print("Hello, "+ full_name.title() + "!")
