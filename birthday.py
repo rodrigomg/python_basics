e605aa0797bbcbfebbcf9906a55227e6c9af9630
technosoul <rockdrigo.mtz@gmail.com>
2020-07-02 23:13:44 -0500

    integers y some uses :smile:

---
+++ birthday.py
@@ -0,0 +1,14 @@
+age = 23
+message = "Happy " + str(age) + "rd Birthday"
+print(message)
+
+print(4+4)
+print(4*2)
+print(16/2)
+print(12-4)
+
+favorite_number = 7
+message = "My favorite number is " + str(favorite_number)
+
+print(message)
+
