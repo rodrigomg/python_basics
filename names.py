6bf0134f686737f270c7156c8def61bcca4d9387
technosoul <rockdrigo.mtz@gmail.com>
2020-07-06 16:56:55 -0500

    doing some examples

---
+++ names.py
@@ -0,0 +1,9 @@
+names = ['Richie','George','Gogdito','Sien','Lalo','Pepito','Gus','Xavo']
+message = "Hi dear "
+print(message + names[0])
+print(message + names[1])
+print(message + names[2])
+print(message + names[3])
+print(message + names[4])
+print(message + names[5])
+print(message + names[-1])
