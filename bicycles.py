793b4002774e5c0f39eb40f38fcd1b7d67b84024
technosoul <rockdrigo.mtz@gmail.com>
2020-07-05 22:57:28 -0500

    playing with index :P

--- bicycles.py
+++ bicycles.py
@@ -1,3 +1,10 @@
 bicycles = ['trek','cannondale','redline','specialized']
 print(bicycles)
 print(bicycles[0].title())
+print(bicycles[1].title())
+print(bicycles[3].title())
+print(bicycles[-1].title())
+print(bicycles[-2].title())
+print(bicycles[-3].title())
+print(bicycles[-4].title())
+
