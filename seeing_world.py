9fcf483f399cef4af0f2bb756b624b5698c7c708
technosoul <rockdrigo.mtz@gmail.com>
2020-07-12 14:45:05 -0500

    using sorted reverse :P

--- seeing_world.py
+++ seeing_world.py
@@ -1,3 +1,6 @@
 places_like_to_visit = ['Huatuco','Grecia','Manchester','Los cabos','Avellaneda']
+print(places_like_to_visit)
 print(sorted(places_like_to_visit))
+print(places_like_to_visit)
+print(sorted(places_like_to_visit,reverse=True))
 print(places_like_to_visit)
