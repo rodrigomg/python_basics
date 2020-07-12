054b0401baee512ec5481781c6b48bb9881fb64e
technosoul <rockdrigo.mtz@gmail.com>
2020-07-12 08:49:37 -0500

    playing with list and some methods :beer:

--- cars.py
+++ cars.py
@@ -11,3 +11,10 @@ print("\nHere´s the sorted list:")
 print(sorted(cars))
 print("\nHere´s the original list again:")
 print(cars)
+
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("\nreverse:")
+print(cars)
+cars.reverse()
+print(cars)
---
+++ seeing_world.py
@@ -0,0 +1,3 @@
+places_like_to_visit = ['Huatuco','Grecia','Manchester','Los cabos','Avellaneda']
+print(sorted(places_like_to_visit))
+print(places_like_to_visit)
