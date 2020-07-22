777f449811cee18a742b6744339a166f91d89178
technosoul <rockdrigo.mtz@gmail.com>
2020-07-21 20:44:46 -0500

    one million baby :smile:

--- loops.py
+++ loops.py
@@ -1,3 +1,10 @@
 for number in range(1,21):
     print(number)
 
+one_million = range(1,10000001)
+#for number in one_million:
+#    print(number)
+
+print(min(one_million))
+print(max(one_million))
+
