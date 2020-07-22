b95da9919b7b03da48a0462b97e8755aed240b5e
technosoul <rockdrigo.mtz@gmail.com>
2020-07-21 21:02:00 -0500

    multiples of three

--- loops.py
+++ loops.py
@@ -9,6 +9,9 @@ print(min(one_million))
 print(max(one_million))
 
 odd_numbers = range(1,20,2)
-
 for odd_number in odd_numbers:
     print(odd_number)
+
+multiples_of_three = range(3,30,3)
+for multiple_of_three in multiples_of_three:
+    print(multiple_of_three)
