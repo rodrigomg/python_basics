9610fba12faa685c0b8894896af5cd6cc133260b
technosoul <rockdrigo.mtz@gmail.com>
2020-07-23 00:16:42 -0500

    copy list example

---
+++ foods.py
@@ -0,0 +1,9 @@
+my_foods = ['pizza','falafel','carrot cake']
+friend_foods = my_foods[:]
+
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend´s favorite foods are:")
+print(friend_foods)
