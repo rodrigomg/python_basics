eddbaca0fcd8a1e1cdbc38e6833a685f07d20706
technosoul <rockdrigo.mtz@gmail.com>
2020-07-23 00:03:11 -0500

    the firts three players

--- players.py
+++ players.py
@@ -3,6 +3,12 @@ print(players[0:3])
 print(players[1:4])
 print(players[:4])
 print(players[2:])
+print(players[-3:])
+
+print("Here are the firts three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
 
 
 
