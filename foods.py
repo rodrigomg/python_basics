21e18499d2fc59dea4fc524181b12d714def381f
technosoul <rockdrigo.mtz@gmail.com>
2020-07-25 11:09:51 -0500

    bad copy O.O

--- foods.py
+++ foods.py
@@ -1,5 +1,8 @@
 my_foods = ['pizza','falafel','carrot cake']
+
 friend_foods = my_foods[:]
+# Don´t do this ¬¬
+#friend_foods = my_foods
 
 my_foods.append('cannoli')
 friend_foods.append('ice cream')
