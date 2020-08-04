d4756e1f764e33291edac3e911f1a286f78ef244
technosoul <rockdrigo.mtz@gmail.com>
2020-08-04 17:02:33 -0500

    adding some list excersice to pizzas

--- pizzas.py
+++ pizzas.py
@@ -3,3 +3,12 @@ pizzas = ['Mexicana','Pepperoni','Pastor','Carnes frías']
 for pizza in pizzas:
     print('I like ' + pizza + " pizza")
 print("I really love pizza!!")
+
+friend_pizzas = pizzas[:]
+friend_pizzas.append("Vegetariana")
+
+for pizza in pizzas:
+    print("My favorite pizzas are:" + pizza)
+
+for pizza in friend_pizzas:
+    print("My friend´s favorite pizzas are:" + pizza)
