7a36bf628a82528ecbf7acb00564563b39e6964e
technosoul <rockdrigo.mtz@gmail.com>
2020-08-05 20:48:16 -0500

    some usages of tuples :rocket:

--- dimensions.py
+++ dimensions.py
@@ -2,3 +2,14 @@ dimensions = (200,50)
 print(dimensions[0])
 print(dimensions[1])
 
+#Tuple are immutable
+#dimensions[0] = 250
+
+print("Original dimension")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100)
+print("\nModified dimension")
+for dimension in dimensions:
+    print(dimension)
