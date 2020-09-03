d25938e1d4e63de2fc727d8373026e63f1c05feb
technosoul <rockdrigo.mtz@gmail.com>
2020-09-02 23:07:34 -0500

    more conditional examples :smile:

--- conditional_test.py
+++ conditional_test.py
@@ -4,3 +4,20 @@ print(car == 'subaru')
 
 print("Is car == 'audi'? I predict False.")
 print(car == 'audi')
+
+my_number = 7
+
+print("Is my number  > '4'? and < 6 I predict True.")
+print((my_number > 4 and my_number < 8))
+
+my_love_team = 'Guadalajara'
+
+
+print("Is my team == 'Real Madrid'")
+print(my_love_team == 'Real Madrid')
+
+print("Is my team == 'Guadalajara'")
+print(my_love_team == 'guadalajara'.title())
+
+print("Is my team == 'Guadalajara' or 'Manchester United'")
+print(my_love_team == 'guadalajara'.title() or my_love_team == 'Manchester United')
