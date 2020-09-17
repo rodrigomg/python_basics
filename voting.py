f86af9d9282047178759a3c71353df1642e825da
technosoul <rockdrigo.mtz@gmail.com>
2020-09-16 22:28:01 -0500

    adding an simple else :smile:

--- voting.py
+++ voting.py
@@ -1,4 +1,8 @@
-age = 19
+age = 17
 if age >= 18:
     print("You`re old enough to vote!")
     print("Have you registered to vote yet?")
+else:
+    print("Sorry, you´re too youg to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
