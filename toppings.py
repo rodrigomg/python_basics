608e0dd18acb4052a73f15058330936e4016388e
technosoul <rockdrigo.mtz@gmail.com>
2020-09-20 18:37:29 -0500

    multiple if exmaple

---
+++ toppings.py
@@ -0,0 +1,10 @@
+requested_toppings = ['mushrooms','extra_cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra_cheese' in requested_toppings:
+    print("Adding extra_cheese.")
+
+print("\nFinished making your pizza!!")
