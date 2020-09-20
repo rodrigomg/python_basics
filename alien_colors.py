3cfd63344e4131bba01b0134542d716d1058c58b
technosoul <rockdrigo.mtz@gmail.com>
2020-09-20 18:50:02 -0500

    alien colors example

---
+++ alien_colors.py
@@ -0,0 +1,12 @@
+#alien_colors = ['green','yellow','red']
+
+alien = 'yellow'
+
+if alien == 'green':
+    points = 5
+elif alien == 'yellow':
+    points = 10
+elif alien == 'red':
+    points = 15
+
+print("The player got " + str(points) + ".")
