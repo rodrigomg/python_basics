adaf648c79c1058156477ac737d373a4e1ce5167
technosoul <rockdrigo.mtz@gmail.com>
2020-09-20 18:24:20 -0500

    using price varible

--- amusement_park.py
+++ amusement_park.py
@@ -1,9 +1,11 @@
 age = 12
 
 if age < 4:
-     print("Your admission cost is $0.")
+    price = 0
 elif age < 18:
-     print("Your admission cost is $5.")
+    price = 5
 else:
-     print("Your admission cost is $10.")
+    price = 10
+
+print("Your admission cost is " + str(price) + ".")
 
