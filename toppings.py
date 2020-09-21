6aa778989f3f7f643aa7889c288a7b53c0387cfa
technosoul <rockdrigo.mtz@gmail.com>
2020-09-20 19:23:47 -0500

    validating an empty list

--- toppings.py
+++ toppings.py
@@ -7,10 +7,14 @@ if 'pepperoni' in requested_toppings:
 if 'extra_cheese' in requested_toppings:
     print("Adding extra_cheese.")
 
-for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers':
-        print("Sorry, we are out of green peppers right now")
-    else:
-        print("Adding " + requested_topping + ".")
+requested_toppings = []
 
-print("\nFinished making your pizza!!")
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            print("Sorry, we are out of green peppers right now")
+        else:
+            print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!!")
+else:
+    print("Are you sure you want a plain pizza?")
