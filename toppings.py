dba9e2efccc7e0f44a9a8cf80b15a5a135a1047b
technosoul <rockdrigo.mtz@gmail.com>
2020-09-20 19:20:30 -0500

    using a for and an if to topping example

--- toppings.py
+++ toppings.py
@@ -1,4 +1,4 @@
-requested_toppings = ['mushrooms','extra_cheese']
+requested_toppings = ['mushrooms','green peppers','extra_cheese']
 
 if 'mushrooms' in requested_toppings:
     print("Adding mushrooms.")
@@ -6,5 +6,11 @@ if 'pepperoni' in requested_toppings:
     print("Adding pepperoni.")
 if 'extra_cheese' in requested_toppings:
     print("Adding extra_cheese.")
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now")
+    else:
+        print("Adding " + requested_topping + ".")
 
 print("\nFinished making your pizza!!")
