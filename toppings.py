1ecb1e07dc25e4538f83773ee79a8518d6c71590
technosoul <rockdrigo.mtz@gmail.com>
2020-09-20 19:32:01 -0500

    using a multiple list

--- toppings.py
+++ toppings.py
@@ -18,3 +18,14 @@ if requested_toppings:
     print("\nFinished making your pizza!!")
 else:
     print("Are you sure you want a plain pizza?")
+
+available_toppings = ['mushrooms', 'olives','green peppers','pepperoni','pineapple','extra cheese']
+requested_toppings = ['mushrooms','french fies','extra chesse']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don´t have " + requested_topping +".")
+
+print("\nFinished making your pizza!!")
