1af18df538503f3d92dc89570ad493373b159a02
technosoul <rockdrigo.mtz@gmail.com>
2020-09-26 15:57:26 -0500

    adding elemnts to dictionary, starting with an empty dictionary

--- alien.py
+++ alien.py
@@ -2,6 +2,19 @@ alien_O = {'color':'green','points':5}
 
 print(alien_O['color'])
 print(alien_O['points'])
+print(alien_O)
+
+alien_O['x_position'] = 0
+alien_O['y_position'] = 25
+print(alien_O)
+
 
 new_points = alien_O['points']
 print("You just earned " + str(new_points) + " points")
+
+alien_E = {}
+
+alien_E['color'] = 'green'
+alien_E['points'] = 5
+
+print(alien_E)
