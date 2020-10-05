6d1f3529cf6015130a922ea5ffb4ebff47ffcf16
technosoul <rockdrigo.mtz@gmail.com>
2020-10-04 21:21:50 -0500

    alien position modified :smile:

--- alien.py
+++ alien.py
@@ -22,3 +22,23 @@ print(alien_E)
 alien_E['color'] = 'yellow'
 
 print("The alien is now " + alien_E['color'] +".")
+
+alien_P = {'x_position':0, 'y_position':25,'speed':'medium'}
+print("Original x-position: " + str(alien_P['x_position']))
+
+# Move the alien to the right.
+# Determine how far to move the alien bades on its current speed.
+
+if alien_P['speed'] == 'slow':
+    x_increment = 1
+elif alien_P['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+
+# Thw new position is de old position plus the increment.
+
+alien_P['x_position'] =alien_P['x_position'] + x_increment
+
+print("New x_position: " + str(alien_P['x_position']))
