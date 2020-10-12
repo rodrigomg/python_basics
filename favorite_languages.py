6283fc0e97a44cd80dc13d9617cf273ec8fb5803
technosoul <rockdrigo.mtz@gmail.com>
2020-10-11 20:45:07 -0500

    favorite languages example

---
+++ favorite_languages.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+        'jen':'python',
+        'sarah':'c',
+        'edward':'ruby',
+        'phil':'ptthon',
+        }
+print(favorite_languages)
+
+print("Sarah´s favorite language is " +
+        favorite_languages['sarah'].title() +
+        ".")
