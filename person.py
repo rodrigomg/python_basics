c712a5c7b779ae0c567cb6426d31817ad498694b
technosoul <rockdrigo.mtz@gmail.com>
2020-10-12 23:53:53 -0500

    a small dictionary example :smile:

---
+++ person.py
@@ -0,0 +1,14 @@
+person_information = {"first_name":"Rodrigo",
+        "last_name":"Martinez Garcia",
+        "age":37,
+        "city":"CDMX",}
+print(person_information)
+print(person_information["age"])
+
+favorite_numbers = {"Richie": 11,
+        "George": 9,
+        "Gus":10,
+        "Cien":5,
+        "Xavo":4,}
+print(favorite_numbers)
+print(favorite_numbers["Gus"])
