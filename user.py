66f1230a37cfcf34498d32b2495d4935856b7607
technosoul <rockdrigo.mtz@gmail.com>
2020-10-20 20:54:06 -0500

    using a loop to discover directory content

---
+++ user.py
@@ -0,0 +1,9 @@
+user_O = {
+        'username':'efermi',
+        'first':'enrico',
+        'last':'fermi',
+        }
+
+for k,v in user_O.items():
+    print("\nKey:" + k)
+    print("\nValue:" + v)
