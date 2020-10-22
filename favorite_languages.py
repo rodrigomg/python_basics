62ef0debd2991edf9d15693a18fba42ff0fcc601
technosoul <rockdrigo.mtz@gmail.com>
2020-10-21 19:45:44 -0500

    using key method to iterate through the dictionary

--- favorite_languages.py
+++ favorite_languages.py
@@ -8,6 +8,12 @@ favorite_languages = {
 for name, language in favorite_languages.items():
     print(name.title() + "s favorite language is " + language.title()+ ".")
 
+for name in favorite_languages.keys():
+    print(name.title())
+
+for name in favorite_languages:
+    print(name.title())
+
 print(favorite_languages)
 
 print("Sarah´s favorite language is " +
