64bdb656c893bbe503b9e5b0c64756106aa02764
technosoul <rockdrigo.mtz@gmail.com>
2020-11-02 18:58:19 -0600

    change a part of a list

--- aliens.py
+++ aliens.py
@@ -13,8 +13,15 @@ for alien_number in range(30):
     new_alien = {'color':'blue','points':5,'speed':'slow'}
     aliens.append(new_alien)
 
+for alien in aliens[0:3]:
+    if alien['color'] == 'blue':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
 for alien in aliens[:5]:
     print(alien)
 print("...")
 
 print("Total number of aliens:" + str(len(aliens)))
+
